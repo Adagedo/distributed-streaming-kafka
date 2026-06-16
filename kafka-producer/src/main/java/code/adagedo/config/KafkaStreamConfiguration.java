801b4c81317ea5e71c19b85fa87bb803569224f9
Adagedo solomon Israel <adagedosolomon52@gmail.com>
2026-06-17 00:44:46 +0100

    added stream configuration

---
+++ kafka-producer/src/main/java/code/adagedo/config/KafkaStreamConfiguration.java
@@ -0,0 +1,67 @@
+package code.adagedo.config;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.processor.WallclockTimestampExtractor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
+import org.springframework.kafka.config.StreamsBuilderFactoryBeanConfigurer;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+@EnableKafkaStreams
+public class KafkaStreamConfiguration {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaStreamConfiguration.class);
+
+
+    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+    public KafkaStreamsConfiguration kStreamsConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "testStreams");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Integer().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, WallclockTimestampExtractor.class.getName());
+        return new KafkaStreamsConfiguration(props);
+    }
+
+    @Bean
+    public StreamsBuilderFactoryBeanConfigurer configurer() {
+        return factoryBean -> factoryBean.setStateListener(
+                ((newState, oldState) -> {
+                    LOGGER.info("state transition from {} to {}", oldState, newState );
+                })
+        );
+    }
+
+    @Bean
+    public KStream<Integer, String> kStream(StreamsBuilder streamsBuilder) {
+        KStream<Integer, String> stream = streamsBuilder.stream("streaming-recent-changes");
+        stream
+                .mapValues((ValueMapper<String, String>) String::toUpperCase)
+                .groupByKey()
+                .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofMillis(1_000)))
+                .reduce((String value1, String value2) -> value1 + value2,
+                        Named.as("windowStore"))
+                .toStream()
+                .map((windowedId, value) -> new KeyValue<>(windowedId.key(), value))
+                .filter((i, s) -> s.length() > 40)
+                .to("streamingTopic2");
+        stream.print(Printed.toSysOut());
+        return stream;
+    }
+}
--- kafka-producer/src/main/java/code/adagedo/config/handler/EventsChangeHandler.java
+++
@@ -1,6 +0,0 @@
-package code.adagedo.config.handler;
-
-import com.launchdarkly.eventsource.StreamEvent;
-
-public class EventsChangeHandler implements StreamEvent {
-}
