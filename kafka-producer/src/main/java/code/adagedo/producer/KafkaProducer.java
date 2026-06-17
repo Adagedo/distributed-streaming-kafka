5442dda4d3e7740eda3e7f16d463347838d68a91
Adagedo solomon Israel <adagedosolomon52@gmail.com>
2026-06-17 02:08:48 +0100

    eventsource and handlers implementation

--- kafka-producer/src/main/java/code/adagedo/producer/KafkaProducer.java
+++ kafka-producer/src/main/java/code/adagedo/producer/KafkaProducer.java
@@ -1,19 +1,46 @@
 package code.adagedo.producer;
 
-import lombok.AllArgsConstructor;
+import code.adagedo.handler.WikimediaHandler;
+import com.launchdarkly.eventsource.ConnectStrategy;
+import com.launchdarkly.eventsource.EventSource;
+import com.launchdarkly.eventsource.StreamException;
+import com.launchdarkly.eventsource.background.BackgroundEventHandler;
+import com.launchdarkly.eventsource.background.BackgroundEventSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
 
 @Service
-@AllArgsConstructor
-public class KafkaProducer {
+public class KafkaProducer{
 
     private final KafkaTemplate<String, String> template;
+    private final String wikimediaUrl;
 
-    void publishEvents(String message){
+    public KafkaProducer(KafkaTemplate<String, String> template, @Value("${wikimedia.url}") String wikimediaUrl) {
+        this.template = template;
+        this.wikimediaUrl = wikimediaUrl;
+    }
+
+    public void publishEvents(String message) throws StreamException, InterruptedException {
         String topic = "streaming-recent-changes";
-        template.send(topic, message);
+
+        BackgroundEventHandler eventHandler = new WikimediaHandler(template, topic);
+        EventSource eventSourceBuilder = buildSource(eventHandler);
+        eventSourceBuilder.start();
+        Thread.sleep(Long.MAX_VALUE);
+    }
+
+    private EventSource buildSource(BackgroundEventHandler backgroundEventHandler){
+        BackgroundEventSource eventSource = new BackgroundEventSource.Builder(
+                backgroundEventHandler,
+                new EventSource.Builder(ConnectStrategy.http(URI.create(wikimediaUrl)).connectTimeout(5, TimeUnit.SECONDS))
+        ).build();
+
+        return eventSource.getEventSource();
     }
 }
 //https://stream.wikimedia.org/v2/stream/recentchange
