cd413eb31b7d0af28e2f7087f085cb34468eb46c
Adagedo solomon Israel <adagedosolomon52@gmail.com>
2026-06-18 00:54:49 +0100

    created the consumer module

---
+++ kafka-comsumer/src/main/java/code/adagedo/ConsumerApplication.java
@@ -0,0 +1,16 @@
+package code.adagedo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class ConsumerApplication
+{
+    public static void main( String[] args ) {
+        SpringApplication.run(ConsumerApplication.class);
+    }
+}
