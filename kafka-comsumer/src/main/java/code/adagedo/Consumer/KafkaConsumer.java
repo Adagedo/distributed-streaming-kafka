9b57f5f60445c25dcb9e5f247fb21f6202f5034d
Adagedo solomon Israel <adagedosolomon52@gmail.com>
2026-06-19 00:53:01 +0100

    save event to db

--- kafka-comsumer/src/main/java/code/adagedo/Consumer/KafkaConsumer.java
+++ kafka-comsumer/src/main/java/code/adagedo/Consumer/KafkaConsumer.java
@@ -1,18 +1,25 @@
 package code.adagedo.Consumer;
 
+import code.adagedo.models.StreamData;
+import code.adagedo.repository.StreamRepository;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class KafkaConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
 
+    private final StreamRepository repository;
+
     @KafkaListener(topics = "distributed_streaming", groupId = "realtime-stream-v2")
     public void consume(String eventMessage) {
-        LOGGER.info("Event message received, {}", eventMessage);
-        System.out.println("Event message received: " + eventMessage );
+        StreamData data = new StreamData();
+        data.setWikiEventData(eventMessage);
+        repository.save(data);
     }
 }
--- kafka-comsumer/src/main/java/code/adagedo/models/StreamData.java
+++ kafka-comsumer/src/main/java/code/adagedo/models/StreamData.java
@@ -1,6 +1,7 @@
 package code.adagedo.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -14,20 +15,8 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @Table
 public class StreamData extends BaseModel{
-    private Meta meta;
-    private String type;
-    private Integer namespace;
-    private String title;
-    private String titleUrl;
-    private String comment;
-    private Integer timestamp;
-    private String user;
-    private Boolean bot;
-    private String notifyUrl;
-    private String serverUrl;
-    private String serverName;
-    private String serverScriptPath;
-    private String wiki;
-    private String parsedcomment;
+
+    @Lob
+    private String wikiEventData;
 
 }
