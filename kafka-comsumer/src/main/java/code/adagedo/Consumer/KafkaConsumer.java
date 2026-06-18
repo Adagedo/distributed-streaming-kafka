f43057eb524d1188a86a682480a84064a1aafba6
Adagedo solomon Israel <adagedosolomon52@gmail.com>
2026-06-18 09:05:43 +0100

    removed comments

---
+++ kafka-comsumer/src/main/java/code/adagedo/Consumer/KafkaConsumer.java
@@ -0,0 +1,18 @@
+package code.adagedo.Consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = "streaming-recent-changes", groupId = "realtime-stream")
+    public void consume(String eventMessage) {
+        LOGGER.info("Event message received, {}", eventMessage);
+        System.out.println("Event message received: " + eventMessage );
+    }
+}
